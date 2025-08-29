e4457e9af422957cd01c1ab7f722a4d7ea5a699c
nariakiiwatani <nariakiiwatani@annolab.com>
2025-08-29 15:46:41 +0900

    update CMakeLists

---
+++ current/build/CMakeFiles/urg_c.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/urg_c.dir/src/urg_connection.c.o"
+  "CMakeFiles/urg_c.dir/src/urg_connection.c.o.d"
+  "CMakeFiles/urg_c.dir/src/urg_debug.c.o"
+  "CMakeFiles/urg_c.dir/src/urg_debug.c.o.d"
+  "CMakeFiles/urg_c.dir/src/urg_ring_buffer.c.o"
+  "CMakeFiles/urg_c.dir/src/urg_ring_buffer.c.o.d"
+  "CMakeFiles/urg_c.dir/src/urg_sensor.c.o"
+  "CMakeFiles/urg_c.dir/src/urg_sensor.c.o.d"
+  "CMakeFiles/urg_c.dir/src/urg_serial.c.o"
+  "CMakeFiles/urg_c.dir/src/urg_serial.c.o.d"
+  "CMakeFiles/urg_c.dir/src/urg_serial_utils.c.o"
+  "CMakeFiles/urg_c.dir/src/urg_serial_utils.c.o.d"
+  "CMakeFiles/urg_c.dir/src/urg_serial_utils_linux.c.o"
+  "CMakeFiles/urg_c.dir/src/urg_serial_utils_linux.c.o.d"
+  "CMakeFiles/urg_c.dir/src/urg_tcpclient.c.o"
+  "CMakeFiles/urg_c.dir/src/urg_tcpclient.c.o.d"
+  "CMakeFiles/urg_c.dir/src/urg_utils.c.o"
+  "CMakeFiles/urg_c.dir/src/urg_utils.c.o.d"
+  "liburg_c.a"
+  "liburg_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/urg_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ current/build/CMakeFiles/urg_c.dir/src/urg_ring_buffer.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/urg_c.dir/src/urg_ring_buffer.c.o: \
+  /Users/nariakiiwatani/Works/HokuyoHub/proj/external/urg_library/current/src/urg_ring_buffer.c \
+  /Users/nariakiiwatani/Works/HokuyoHub/proj/external/urg_library/current/include/c/urg_ring_buffer.h
